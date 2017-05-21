ebb69c78d08a61eaa0d2b8f8f8adc580e772aca3
Михаил <Mihail.sverkynov@gmail.com>
2017-05-22 06:36:57 +0700

    asdfasdfa

--- ViewModel/ChangeProfileViewModel.cs
+++ ViewModel/ChangeProfileViewModel.cs
@@ -4,11 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace ViewModel
 {
     public class ChangeProfileViewModel : ViewModelBase
     {
+        public IUser ChangingSettings { get; set; }
+
+
+        private RelayCommand _saveClick;
+        public RelayCommand SaveClick
+        {
+            get { return _saveClick ?? (_saveClick = new RelayCommand(OnSaveClick)); }
+        }
+        private void OnSaveClick()
+        {
+            
+            Close();
+        }
+
+
         private RelayCommand _cancelClick;
         public RelayCommand CancelClick
         {
